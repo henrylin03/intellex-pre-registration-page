8859a432966ef3aa323ae483987a29e0e7b10f01
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-09-09 18:00:30 +1000

    feat: users are able to toggle between english and simplified chinese using the button group on the header. this header is on all pages. update the title (what is seen in the browser tab) using | symbol instead of colon to align with rest of the site

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const dialog = document.querySelector("dialog");
 const joinButton = document.querySelector("#join-waitlist");
 const emailButton = document.querySelector("#email");
+const switchLanguageButtons = document.querySelectorAll(
+  ".switch-language-button"
+);
 
 // event handlers
 const closeDialog = (e) => {
@@ -14,7 +17,34 @@ const sendEmail = () => {
   window.location.href = emailLink;
 };
 
+const switchLanguage = (e) => {
+  const selectedLanguage = e.target.dataset.language;
+  const variableLanguageElements = {
+    english: document.querySelectorAll("body [lang='en']"),
+    simplifiedChinese: document.querySelectorAll("body [lang='zh']"),
+  };
+
+  if (!(selectedLanguage in variableLanguageElements)) return;
+
+  // update ui styling for buttons
+  switchLanguageButtons.forEach((button) =>
+    button.classList.remove("selected")
+  );
+  e.target.classList.add("selected");
+
+  for (const [language, elements] of Object.entries(variableLanguageElements)) {
+    if (language === selectedLanguage) {
+      elements.forEach((element) => (element.style.display = "block"));
+    } else {
+      elements.forEach((element) => (element.style.display = "none"));
+    }
+  }
+};
+
 // event listeners
+switchLanguageButtons.forEach((button) =>
+  button.addEventListener("click", switchLanguage)
+);
 joinButton.addEventListener("click", () => dialog.showModal());
 dialog.addEventListener("click", closeDialog);
 emailButton.addEventListener("click", sendEmail);
