6f33cc324beb361289b91457bb19b71ed007e5d7
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-09-10 14:35:27 +1000

    feat: language selection preferences will persist while on any page, not just the landing.
    selecting a language stores the user's preferred language into their localStorage. on load of every page, the preferred language is gotten from localStorage, and page rendered. on first visit of site, there will be no locally-stored preferred language, so English is shown by default. the user no longer has to click their preferred language (presuming non-English) on the header on every page. fixes #22.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-import enableLanguageSwitch from "./enableLanguageSwitch.mjs";
+import {
+  enableLanguageSwitch,
+  renderPageInPreferredLanguage,
+} from "./languageManager.mjs";
 import enableCTAs from "./enableCTAs.mjs";
 
 const isHomepage = () => window.location.pathname === "/";
 
 if (isHomepage()) enableCTAs();
-
 enableLanguageSwitch();
+renderPageInPreferredLanguage();
